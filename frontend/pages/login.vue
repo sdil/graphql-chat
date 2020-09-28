96183c59e05cadffcae69391c2545215d22ba2ac
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-09-28 11:11:29 +0800

    Integrate with google login & fix the env variable load

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <h3 class="display-1 mb-5">Firebase Authentication</h3>
+    Login
     <div class="links">
       <input type="text" v-model="formData.email" />
       <input type="text" v-model="formData.password" />
       <input @click="signInUser" type="submit" />
     </div>
-    {{authUser}}
+    Register
+    <div class="links">
+      <input type="text" v-model="formData.email" />
+      <input type="text" v-model="formData.password" />
+      <input @click="signInUser" type="submit" />
+    </div>
+
+    <input @click="googleLogin" type="submit" />
   </div>
 </template>
 
 
 <script>
+import firebase from "firebase"
+
 export default {
   data() {
     return {
@@ -22,18 +32,33 @@ export default {
     };
   },
   methods: {
+    googleLogin: function (user) {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope("profile");
+      provider.addScope("email");
+      this.$fireAuth.signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(token)
+        console.log(user)
+      });
+    },
     signInUser: function (user) {
       console.log(this.formData.email);
       console.log(this.formData.password);
       try {
-        console.log(this.$fireAuth.signInWithEmailAndPassword(
-          this.formData.email,
-          this.formData.password)
+        console.log(
+          this.$fireAuth.signInWithEmailAndPassword(
+            this.formData.email,
+            this.formData.password
+          )
         );
       } catch (e) {
         alert(e);
       }
-    //   console.log(this.)
+      //   console.log(this.)
     },
   },
 };
