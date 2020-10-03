51bcab6b1e5f45565eb35f2b2589ee0dbb5982b3
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-10-03 19:03:22 +0800

    Add hasura migrations

---
+++ chat/migrations/1601722913324_init/up.sql
@@ -0,0 +1,41 @@
+CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$;
+CREATE TABLE public.message (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    message text NOT NULL,
+    sent_at timestamp with time zone DEFAULT now() NOT NULL,
+    sent_by text NOT NULL,
+    room uuid NOT NULL
+);
+COMMENT ON TABLE public.message IS 'message sent by users';
+CREATE TABLE public.room (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    description text
+);
+CREATE TABLE public."user" (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    username text NOT NULL
+);
+COMMENT ON TABLE public."user" IS 'user details';
+ALTER TABLE ONLY public.message
+    ADD CONSTRAINT message_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.room
+    ADD CONSTRAINT room_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
+CREATE TRIGGER set_public_user_updated_at BEFORE UPDATE ON public."user" FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_user_updated_at ON public."user" IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+ALTER TABLE ONLY public.message
+    ADD CONSTRAINT message_room_fkey FOREIGN KEY (room) REFERENCES public.room(id) ON UPDATE CASCADE ON DELETE CASCADE;
