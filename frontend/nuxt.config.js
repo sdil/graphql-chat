fbb9518b26faf83e3ff353364bb8ced6d649c7f5
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-10-27 11:11:57 +0800

    [frontend] defer scripts loading to improve page loading

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -20,9 +20,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      { src: 'https://js.stripe.com/v3' },
+      { src: 'https://js.stripe.com/v3', defer: true },
       {
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.7.3/lottie.min.js'
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.7.3/lottie.min.js', defer: true
       }
     ]
   },
